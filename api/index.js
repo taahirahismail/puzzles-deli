50489762ec2d02f67603cf32539698b30f56f82e
taahirahismail <taahirah.i@gmail.com>
2023-08-19 10:22:54 +0200

    added more cors stuff

--- api/index.js
+++ api/index.js
@@ -14,7 +14,12 @@ app.use(
     routes
 );
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:8080');
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Allow-Methods", "*");
+    res.header("Access-Control-Request-Methods", "*");
+    res.header("Access-Control-Allow-Headers", "*");
+    res.header("Access-Control-Expose-Headers", "Authorization");
     next();
 });
 
